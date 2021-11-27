f256fb65f6d52d8a9a40ca0e660da4047dd8fa84
tomri <tomrickus@hmail.com>
2021-11-27 11:38:15 +0200

    Mano pirmasis comit

--- src/main/java/example5/Main.java
+++ src/main/java/example5/Main.java
@@ -3,5 +3,12 @@ package example5;
 public class Main {
     public static void main(String[] args) {
 
+        //ObjektoTipas objektoVardas = KlasesKonstruktorius();
+        Robot robotJimmy = new Robot(); // Konstruktorius uskuria atmintyje object instance pagal duota blueprint/brezini
+        robotJimmy.setName("Jimmy");
+        robotJimmy.setPrice(500);
+        robotJimmy.setSeriesNo(1);
+
+        Addon goldenHelmet = new Addon(150, "golden_helmet");
     }
 }
